d7cf3faf37a2b5e17137a29759fdd6caf625a59b
Lucas Iturriago <70216214+liturriago@users.noreply.github.com>
2025-11-13 15:52:40 -0500

    Increase num_workers default in Segmentation_DataLoader
    Changed the default value of num_workers from 0 to 4 in Segmentation_DataLoader to improve data loading performance. Also updated the package version to 0.1.0.69.

--- gcpds_cv_pykit/segmentation/baseline/dataloaders/dataloader.py
+++ gcpds_cv_pykit/segmentation/baseline/dataloaders/dataloader.py
@@ -250,7 +250,7 @@ def Segmentation_DataLoader(
     single_class: Optional[int] = None,
     augment: bool = True,
     images_folder: Optional[str] = None,
-    num_workers: int = 0,
+    num_workers: int = 4,
     prefetch_factor: int = 2,
     pin_memory: bool = True
 ) -> DataLoader:
